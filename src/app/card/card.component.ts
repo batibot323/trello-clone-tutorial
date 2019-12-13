0cb43c4d8bfa17b1880708482945965f7432cfd7
Hani <hani.herbert.ho@gmail.com>
2019-12-13 09:48:34 +0800

    Added cardList and removed unused properties.

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  myCard: any;
+
+  cardList: any[] = [
+    {
+      id: 1,
+      title: "Drink water!",
+      description: "Drink 8 glasses of water! Stay hydrated, king!"
+    },
+    {
+      id: 2,
+      title: "Read a book",
+      description: "It can be any book, a technical book, or just for enjoyment."
+    },
+    {
+      id: 3,
+      title: "Finish Gym Module",
+      description: "Make sure it includes proper routing."
+    }
+  ];
 
   constructor() { }
-
+  
   ngOnInit() {
+    this.myCard = this.cardList.find(card => card.id === 1);
   }
 
 }
