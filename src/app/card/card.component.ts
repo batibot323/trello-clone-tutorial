657a17363fe20e357adfc6c95db43a4e0ea18e98
batibot323 <hani.herbert.ho@gmail.com>
2020-01-29 17:16:40 +0800

    Cleaned up code base.

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -12,18 +12,18 @@ export class CardComponent implements OnInit {
   cardList: any[] = [
     {
       id: 1,
-      title: "Drink water!",
-      description: "Drink 8 glasses of water! Stay hydrated, king!"
+      title: 'Drink water!',
+      description: 'Drink 8 glasses of water! Stay hydrated, king!'
     },
     {
       id: 2,
-      title: "Read a book",
-      description: "It can be any book, a technical book, or just for enjoyment."
+      title: 'Read a book',
+      description: 'It can be any book, a technical book, or just for enjoyment.'
     },
     {
       id: 3,
-      title: "Finish Gym Module",
-      description: "Make sure it includes proper routing."
+      title: 'Finish Gym Module',
+      description: 'Make sure it includes proper routing.'
     }
   ];
 
