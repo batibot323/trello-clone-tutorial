e0394e15bb88b9b628eebaa0eb19c4f165b95fd2
Hani Ho <hani.herbert.ho@gmail.com>
2021-03-18 09:58:30 +0800

    Added more cards in card-data service.

--- src/services/card-data.service.ts
+++ src/services/card-data.service.ts
@@ -24,6 +24,41 @@ export class CardDataService {
       id: 3,
       title: 'Finish Gym Module',
       description: 'Make sure it includes proper routing.'
+    },
+    {
+      id: 4,
+      title: 'Sleep early!',
+      description: 'Sleep is essential!'
+    },
+    {
+      id: 5,
+      title: 'Buy milk and eggs',
+      description: 'Nothing beats tasty sources of protein.'
+    },
+    {
+      id: 6,
+      title: 'Household chores',
+      description: 'Clean up, wash dishes, and sweep the floor.'
+    },
+    {
+      id: 7,
+      title: 'Talk with friends',
+      description: 'You miss them, you know you do.'
+    },
+    {
+      id: 8,
+      title: 'Breathe',
+      description: 'Go relax and do nothing.'
+    },
+    {
+      id: 9,
+      title: 'Review Flash Cards',
+      description: 'It could be about anything, just do it!'
+    },
+    {
+      id: 10,
+      title: 'Play with cat!',
+      description: `Don't forget to send some pictures!`
     }
   ];
 
