eadcf0a394591416a30ebb9e17d92022c4ee8887
Hani Ho <hani.herbert.ho@gmail.com>
2021-03-18 09:35:05 +0800

    Moved cardList from card to card-data service to really improve performance.

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CardDataService } from 'src/services/card-data.service';
 
 @Component({
@@ -10,28 +10,9 @@ export class CardComponent implements OnInit {
   @Input() id: number;
   myCard: any;
 
-  cardList: any[] = [
-    {
-      id: 1,
-      title: 'Drink water!',
-      description: 'Drink 8 glasses of water! Stay hydrated, king!'
-    },
-    {
-      id: 2,
-      title: 'Read a book',
-      description: 'It can be any book, a technical book, or just for enjoyment.'
-    },
-    {
-      id: 3,
-      title: 'Finish Gym Module',
-      description: 'Make sure it includes proper routing.'
-    }
-  ];
-
   constructor(private cardDataService: CardDataService) { }
 
   ngOnInit() {
-    // this.myCard = this.cardList.find(card => card.id === this.id);
-    this.myCard = this.cardDataService.getCard();
+    this.myCard = this.cardDataService.getCard(this.id);
   }
 }
--- src/services/card-data.service.ts
+++ src/services/card-data.service.ts
@@ -9,11 +9,25 @@ export class CardDataService {
     console.log('card-data service is instantiated!');
   }
 
-  getCard(): any {
-    return {
+  cardList: any[] = [
+    {
       id: 1,
       title: 'Drink water!',
       description: 'Drink 8 glasses of water! Stay hydrated, king!'
-    };
+    },
+    {
+      id: 2,
+      title: 'Read a book',
+      description: 'It can be any book, a technical book, or just for enjoyment.'
+    },
+    {
+      id: 3,
+      title: 'Finish Gym Module',
+      description: 'Make sure it includes proper routing.'
+    }
+  ];
+
+  getCard(id): any {
+    return this.cardList.find(card => card.id === id);
   }
 }
